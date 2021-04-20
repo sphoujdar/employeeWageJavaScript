d3be1b9ba9fad5e01cf86f721f5297241f089781
Shubham <sm.phoujdar@gmail.com>
2021-04-20 09:19:20 +0530

    Refactored: Function added to getWorkHours.

--- runnerFile.js
+++ runnerFile.js
@@ -6,29 +6,42 @@ const HOURS_PER_HALF_DAY = 4;
 const WORKING_DAYS_IN_A_MONTH = 20;
 const MAX_WORKING_HOURS_PER_MONTH = 100;
 
+// Local Vars
 var employeeWageForTheMonth = 0;
-var totalHoursWorkedThisMonth = 0;
+var dailyHoursLog = [];
+
+
 for (let days = 0; days < WORKING_DAYS_IN_A_MONTH; days++) {
     console.log('Day ',days+1,':');
     if (Math.floor(Math.random() * 10) % 2 == 0) {
         console.log('Employee is Absent.');    
-    } else if (totalHoursWorkedThisMonth >= MAX_WORKING_HOURS_PER_MONTH){
+        dailyHoursLog.push(0);
+    } else if (getWorkHours() >= MAX_WORKING_HOURS_PER_MONTH){
         console.log('Employee cannot work today as Max working hours achieved.');    
+        dailyHoursLog.push(0);
     } else {
         let currentWage = 0;
         switch(Math.floor(Math.random() * 10) % 2){
             case 0:
+                dailyHoursLog.push(HOURS_PER_HALF_DAY);
                 currentWage = WAGE_PER_HOUR*HOURS_PER_HALF_DAY;
-                totalHoursWorkedThisMonth+=HOURS_PER_HALF_DAY;
-                console.log('Employee earned Rs.', currentWage, 'for half day of work.');
+                console.log('Employee earned Rs.', currentWage, ' for half day of work.');
                 break;
             default:
+                dailyHoursLog.push(HOURS_PER_FULL_DAY);
                 currentWage = WAGE_PER_HOUR*HOURS_PER_FULL_DAY;
-                totalHoursWorkedThisMonth+=HOURS_PER_FULL_DAY;
-                console.log('Employee earned Rs.', currentWage, 'for full day of work.');
+                console.log('Employee earned Rs.', currentWage, ' for full day of work.');
         }
         employeeWageForTheMonth+=currentWage;
     }   
 }
-console.log('Employee Worked for a total of ',totalHoursWorkedThisMonth,' hours this month.');
+console.log('Employee Worked for a total of ',getWorkHours(),' hours this month.');
 console.log('Total Employee Wage for the month is: Rs.',employeeWageForTheMonth,'.');
+
+function getWorkHours(){
+    let hoursWorkedTillNow = 0;
+    dailyHoursLog.forEach((item) => {
+        hoursWorkedTillNow+=item;
+    })
+    return hoursWorkedTillNow;
+}
