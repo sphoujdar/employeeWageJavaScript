a5e70bbbb5ded6b57e6ee955a67c05d4b580cbc1
Shubham <sm.phoujdar@gmail.com>
2021-04-20 06:28:31 +0530

    Added: Switch-Case statement.

--- runnerFile.js
+++ runnerFile.js
@@ -8,10 +8,11 @@ const HOURS_PER_HALF_DAY = 4;
 if (Math.floor(Math.random() * 10) % 2 == 0) {
     console.log('Employee is Absent.');    
 } else {
-    if (Math.floor(Math.random() * 10) % 2 == 0){
-        console.log('Employee earned Rs.', WAGE_PER_HOUR*HOURS_PER_HALF_DAY, 'for half day of work.');
-    }
-    else{
-        console.log('Employee earned Rs.', WAGE_PER_HOUR*HOURS_PER_FULL_DAY, 'for full day of work.');
+    switch(Math.floor(Math.random() * 10) % 2){
+        case 0:
+            console.log('Employee earned Rs.', WAGE_PER_HOUR*HOURS_PER_HALF_DAY, 'for half day of work.');
+            break;
+        default:
+            console.log('Employee earned Rs.', WAGE_PER_HOUR*HOURS_PER_FULL_DAY, 'for full day of work.');
     }
 }
