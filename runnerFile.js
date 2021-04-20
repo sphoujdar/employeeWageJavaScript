40b59d2a22c5b99bb6a63be45ae4fd99781f277d
Shubham <sm.phoujdar@gmail.com>
2021-04-20 07:07:37 +0530

    Added: Custom wage calculation condition added.

--- runnerFile.js
+++ runnerFile.js
@@ -4,24 +4,31 @@ const WAGE_PER_HOUR = 20;
 const HOURS_PER_FULL_DAY = 8;
 const HOURS_PER_HALF_DAY = 4;
 const WORKING_DAYS_IN_A_MONTH = 20;
+const MAX_WORKING_HOURS_PER_MONTH = 100;
 
 var employeeWageForTheMonth = 0;
+var totalHoursWorkedThisMonth = 0;
 for (let days = 0; days < WORKING_DAYS_IN_A_MONTH; days++) {
     console.log('Day ',days+1,':');
     if (Math.floor(Math.random() * 10) % 2 == 0) {
         console.log('Employee is Absent.');    
+    } else if (totalHoursWorkedThisMonth >= MAX_WORKING_HOURS_PER_MONTH){
+        console.log('Employee cannot work today as Max working hours achieved.');    
     } else {
         let currentWage = 0;
         switch(Math.floor(Math.random() * 10) % 2){
             case 0:
                 currentWage = WAGE_PER_HOUR*HOURS_PER_HALF_DAY;
+                totalHoursWorkedThisMonth+=HOURS_PER_HALF_DAY;
                 console.log('Employee earned Rs.', currentWage, 'for half day of work.');
                 break;
             default:
                 currentWage = WAGE_PER_HOUR*HOURS_PER_FULL_DAY;
+                totalHoursWorkedThisMonth+=HOURS_PER_FULL_DAY;
                 console.log('Employee earned Rs.', currentWage, 'for full day of work.');
         }
         employeeWageForTheMonth+=currentWage;
     }   
 }
+console.log('Employee Worked for a total of ',totalHoursWorkedThisMonth,' hours this month.');
 console.log('Total Employee Wage for the month is: Rs.',employeeWageForTheMonth,'.');
