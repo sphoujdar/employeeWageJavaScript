2712981136840950f9dbd2998994d36d21de55e5
Shubham <sm.phoujdar@gmail.com>
2021-04-20 06:01:27 +0530

    Added: First Wage calculation logic.

--- runnerFile.js
+++ runnerFile.js
@@ -1,5 +1,12 @@
+// Global Constants
+
+const WAGE_PER_HOUR = 20;
+const HOURS_PER_FULL_DAY = 8;
+
+
+
 if (Math.floor(Math.random() * 10) % 2 == 0) {
     console.log('Employee is Absent.');    
 } else {
-    console.log('Employee is Present.');
+    console.log('Employee earned Rs.', WAGE_PER_HOUR*HOURS_PER_FULL_DAY, 'for full day of work.');
 }
